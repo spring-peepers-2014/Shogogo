8293a16836de17bea3e4543c36d7d335deed1d96
Ryan Rebo <ryanpatrickrebo@gmail.com>
2014-06-13 18:59:14 -0400

    Add full_page image url

--- giggidy/config/routes.rb
+++ giggidy/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   root to: 'application#index'
+  resources :bands
   
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
